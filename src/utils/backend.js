aad3d8bb3fe1a63e8db07fa5513bd56c3acc33ae
Philipp Piwo <github@piwo.space>
2020-11-01 11:55:39 +0100

    use backend to cache word bazaar data

--- src/utils/backend.js
+++ src/utils/backend.js
@@ -126,6 +126,10 @@ export default class Backend {
 
   static getTokenInfo = async () => backendFetch('tokenCache/tokenInfo');
 
+  static getWordRegistry = async () => backendFetch('tokenCache/wordRegistry');
+
+  static getWordSale = async (address) => backendFetch(`tokenCache/wordSale?address=${address}`);
+
   static getTokenBalances = async (address) => backendFetch(`tokenCache/balances?address=${address}`);
 
   static getWordSaleDetailsByToken = async (address) => backendFetch(`tokenCache/wordSaleByToken?address=${address}`);
@@ -133,6 +137,10 @@ export default class Backend {
   static cacheInvalidateTips = async () => backendFetch('cache/invalidate/tips');
 
   static invalidateTokenCache = async (token) => backendFetch(`cache/invalidate/token/${token}`);
+
+  static invalidateWordSaleCache = async (wordSale) => backendFetch(`cache/invalidate/wordSale/${wordSale}`);
+
+  static invalidateWordRegistryCache = async () => backendFetch('cache/invalidate/wordRegistry');
 
   static getCommentCountForAddress = async (address) => backendFetch(`comment/count/author/${address}`);
 
